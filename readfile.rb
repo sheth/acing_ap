dc4d5b044b8300299fc474db94c673eb2a2858f8
dsheth <sheth@hotmail.com>
2012-01-29 00:27:54 -0500

    First Day

---
+++ readfile.rb
@@ -0,0 +1,33 @@
+#Ruby program to read a file and print the contents with line nos.
+puts         "<units>"
+file = File.new("units.txt", "r")
+line_empty = false
+unit = nil
+section_started = false
+while (line = file.gets.chomp.strip)
+  if line.start_with? "Unit"
+    unit = line.split('-')
+    puts     "  <id>" + unit[0].sub(/ /, '-') + "</id>"
+    puts     "  <title>#{unit[1].chomp.strip}</title>"
+    line_empty = false
+    next
+  elsif line.chomp.empty?
+    line_empty = true  
+    if section_started
+      puts   '  </section>'
+    end  
+    next
+  elsif line_empty
+    puts     "  <section>"
+    puts     "    <id>" + unit[1].chomp.strip.sub(/ /, '-') + "-" + line.sub(/ /, '-') + "</id>"
+    puts     "    <title>#{line}</title>"
+    line_empty = false
+    section_started = true
+    next
+  else 
+    puts     "    <description>#{line}</description>"
+    next
+  end
+end
+file.close
+puts         '</units>'
