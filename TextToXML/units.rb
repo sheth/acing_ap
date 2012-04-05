2ea02eb3a521d1ebe06ee42cc0cd74cf1fbb8e09
Dhaval Sheth <sheth@hotmail.com>
2012-04-04 23:01:59 -0400

    Made dtd printing optional Updated the dtd Updated the code to reflect the dtd

--- TextToXML/units.rb
+++ TextToXML/units.rb
@@ -1,49 +1,59 @@
 #Ruby program to read units.txt and output a xml document to the console.
-puts         '<?xml version="1.0"?>'
-puts         "<!DOCTYPE units ["
-puts         "        <!ELEMENT units (id, title, section+)+>"
-puts         "        <!ELEMENT section (id, title, description+)>"
-puts         "        <!ELEMENT id (#PCDATA)>"
-puts         "        <!ELEMENT title (#PCDATA)>"
-puts         "        <!ELEMENT description (#PCDATA)>"
-puts         "]>"
-puts         "<units>"
+if ARGV[0] == "print_dtd"
+  puts         '<?xml version="1.0"?>'
+  puts         "<!DOCTYPE units ["
+  puts         "        <!ELEMENT units (unit)+>"
+  puts         "        <!ELEMENT unit (id, title, section+)+>"
+  puts         "        <!ELEMENT section (id, title, description+)>"
+  puts         "        <!ELEMENT id (#PCDATA)>"
+  puts         "        <!ELEMENT title (#PCDATA)>"
+  puts         "        <!ELEMENT description (#PCDATA)>"
+  puts         "]>"
+end
 file = File.new("units.txt", "r")
-line_empty = false
+puts         '<units>'
+empty_line = false
 unit = nil
 section_started = false
+unit_started = false
 while (line = file.gets)
   line.chomp!.strip! #remove leading and trailing spaces and new lines.
   if line.start_with? "Unit"
-    puts         '</units>'
-    puts         "<units>"
+    if unit_started
+      puts   '  </unit>'
+    end
+    unit_started = true
+    puts     "  <unit>"
     unit = line.split('-')
-    puts     "  <id>" + unit[0].gsub(/ /, '-') + "</id>"
-    puts     "  <title>#{unit[1].chomp.strip}</title>"
-    line_empty = false
+    puts     "    <id>" + unit[0].gsub(/ /, '-') + "</id>"
+    puts     "    <title>#{unit[1].chomp.strip}</title>"
+    empty_line = false
     next
   elsif line.empty?
-    line_empty = true  
+    empty_line = true  
     if section_started
-      puts   '  </section>'
+      puts   '    </section>'
       section_started = false
     end  
     next
-  elsif line_empty
-    puts     "  <section>"
-    puts     "    <id>" + unit[1].chomp.strip.gsub(/ /, '-') + "-" + line.gsub(/ /, '-') + "</id>"
-    puts     "    <title>#{line}</title>"
-    line_empty = false
+  elsif empty_line
+    puts     "    <section>"
+    puts     "      <id>" + unit[1].chomp.strip.gsub(/ /, '-') + "-" + line.gsub(/ /, '-') + "</id>"
+    puts     "      <title>#{line}</title>"
+    empty_line = false
     section_started = true
     next
   else 
-    puts     "    <description>#{line}</description>"
+    puts     "      <description>#{line}</description>"
     next
   end
 end
 file.close
 if section_started
-  puts       '  </section>'
+  puts       '    </section>'
   section_started = false
 end  
+if unit_started
+  puts       '  </unit>'
+end
 puts         '</units>'
