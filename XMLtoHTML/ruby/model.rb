66058b27baf9a9396f9933b0170614f7190dbd99
Dhaval Sheth <sheth@hotmail.com>
2012-05-29 01:35:15 -0400

    now it emits ios5 html. Previous version was emitting ios4 html

--- XMLtoHTML/ruby/model.rb
+++ XMLtoHTML/ruby/model.rb
@@ -6,44 +6,63 @@ filename = "../../output/models.xml"
 file = File.new(filename)
 doc = REXML::Document.new file
 puts %{
-<div id="models">
-  <div class="toolbar">
-    <h1>Models</h1>
-    <a href="#" class="back">Back</a>
-  </div>
-  <ul class="edgetoedge">
+      <div data-role="page" id="models">
+        <div data-role="content" data-theme="a">
+          <h3>Models</h3>
      }
 #we will have to traverse twice, first to provide the links and second will have the description
 doc.elements.each("models/model") do |model_element|
   id = model_element.elements['id'].get_text
   title = model_element.elements['title'].get_text
-  puts "<li><a href=\"##{id}\">#{title}</a></li>"
+  puts "<a href=\"##{id}\" data-role=\"button\" data-transition=\pop\ data-theme=\"c\">#{title}</a>"
 end
 puts %{
-  </ul>
+    <a href="#" data-role="button" data-rel="back" data-theme="a">Back</a>
+  </div>
 </div>
      }
-doc.elements.each("models/model") do |model_element|
-  id = model_element.elements['id'].get_text
-  puts "<div id=\"#{id}\">"
-  puts %{ <div class="toolbar">
-            <h1>Models</h1>
-              <a href="#" class="back">Back</a>
-          </div>
-          <div class="selectable">
-       }
-  title = model_element.elements['title'].get_text
-  puts "    <h1 style=\"color:white\">#{title}l</h1> <br/>"
+#get the array this time, as we need to create link to the next model
+model_array = doc.elements.to_a("models/model")
+for i in 0...model_array.size do
+  if (i < (model_array.size - 1))
+    next_model_exists = true
+  else
+    next_model_exists = false
+  end
+  id = model_array[i].elements['id'].get_text
+  title = model_array[i].elements['title'].get_text
   begin
-    image = model_element.elements['image'].get_text.to_s
+    image = model_array[i].elements['image'].get_text.to_s
   rescue
     image = nil
   end
-  if !image.nil?
-    puts "  <img src=\"#{image}\" alt=\"#{title}\" align=\"middle\"/>"
+  des = model_array[i].elements['description'].get_text
+  puts %{
+    <div data-role="page" id="#{id}" data-theme="c">
+     <div data-role="header" data-theme="c" data-position="fixed">
+        <a href="learn.html" data-icon="home" data-transition="pop">Learn it!</a>
+        <h1>Models</h1>
+        <div data-role="navbar">
+           <ul>
+              <li><a href="#" data-rel="back">Back</a></li>
+  }
+  if next_model_exists
+    sibling_id = model_array[i+1].elements['id'].get_text
+    puts "              <li><a href=\"##{sibling_id}\">Next</a></li>"
   end
-  des = model_element.elements['description'].get_text
-  puts "    <p style=\"margin-left: 10px; margin-right: 10px;\">#{des}</p>"
-  puts "  </div>"
-  puts "</div>"
+  puts %{
+           </ul>
+         </div>
+      </div>
+      <div data-role="content" data-theme="c">
+        <h1>#{title}</h1>
+  }
+  if !image.nil?
+    puts "        <img src=\"#{image}\" alt=\"#{title}\" align=\"middle\" width=\"100%\"/>"
+  end
+  puts %{
+      <p style="margin-left: 10px; margin-right: 10px;">#{des}</p>
+   </div>
+</div>
+       }
 end
