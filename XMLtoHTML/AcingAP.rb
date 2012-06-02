f6ea7a51b0d014538116042b2d36d95e42068721
Dhaval Sheth <sheth@hotmail.com>
2012-06-02 19:35:39 -0400

    deleted ios5friendly. Also refactored html methods and moved them to AcingAP module

---
+++ XMLtoHTML/AcingAP.rb
@@ -0,0 +1,31 @@
+module AcingAP
+#common routines used and refactored in this module
+
+  # open_head outputs <html><head>... Does not close the head tag
+  def open_head
+    puts %{
+      <!DOCTYPE html>
+      <html>
+        <head>
+        <title>Acing AP Human Geography</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/themes/default/jquery.mobile-1.1.0.min.css" />
+        <script src="js/jquery-1.7.1.min.js"></script>
+        <script src="js/jquery.mobile-1.1.0.min.js"></script>
+    }
+  end
+
+  def footer
+    puts %{
+      <div data-role="footer" data-position="fixed" data-id="mainFooter">
+        <div data-role="navbar" data-iconpos="top">
+          <ul>
+            <li><a href="learn.html" data-ajax="false" data-icon="grid" data-theme="c">Learn it!</a></li>
+            <li><a href="review.html" data-ajax="false" data-icon="star">Review it!</a></li>
+            <li><a href="take.html" data-ajax="false" data-icon="check">Take it!</a></li>
+          </ul>
+        </div>
+      </div>
+    }
+  end
+end
